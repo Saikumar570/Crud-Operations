693f0bfd645534774181d9dd901260d357854dca
Saikumar570 <sapotnuru@zetaglobal.com>
2024-05-27 17:30:41 +0530

    Update main.go

--- API_USER/main.go
+++ API_USER/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"api/controllers"
 	"api/initializers"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,11 +12,10 @@ func init() {
 }
 func main() {
 	r := gin.Default()
-
 	r.POST("/posts", controllers.Create)
 	r.PUT("/posts/:id", controllers.Update)
 	r.GET("/posts", controllers.READALL)
 	r.GET("/posts/:id", controllers.READONE)
 	r.DELETE("/posts/:id", controllers.Deletes)
-	r.Run() // listen and serve
+	r.Run() 
 }
