8f9cb40c38e31f88b5ed71c2b6d75b81e87a9be7
Saikumar570 <sapotnuru@zetaglobal.com>
2024-06-06 15:47:48 +0530

    Update main.go

--- API_USER/main.go
+++ API_USER/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"api/controllers"
 	"api/initializers"
+	"api/repositories"
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,12 +13,21 @@ func init() {
 	initializers.LoadEnvVariables()
 	initializers.ConnectToDB()
 }
+
 func main() {
+	err := initializers.ConnectToDB()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	postRepository := repositories.FunctionOfRepository(initializers.DB)
+	controllers.PostRepository(postRepository)
 	r := gin.Default()
+
 	r.POST("/posts", controllers.Create)
+	r.GET("/posts", controllers.ReadAll)
+	r.GET("/posts/:id", controllers.ReadOne)
 	r.PUT("/posts/:id", controllers.Update)
-	r.GET("/posts", controllers.READALL)
-	r.GET("/posts/:id", controllers.READONE)
 	r.DELETE("/posts/:id", controllers.Deletes)
-	r.Run() 
+
+	r.Run(":3000")
 }
