0e0e33499554f42050e2536d10ea4ce207003154
Saikumar570 <sapotnuru@zetaglobal.com>
2024-06-11 15:18:04 +0530

    Update postrepository.go

--- API_USER/repositories/postrepository.go
+++ API_USER/repositories/postrepository.go
@@ -7,29 +7,71 @@ import (
 )
 
 type Repository struct {
+	DB      *gorm.DB
+	Post    *PostRepository
+	Comment *CommentRepository
+}
+
+func NewRepository(db *gorm.DB) *Repository {
+	return &Repository{
+		DB:      db,
+		Post:    NewPostRepository(db),
+		Comment: NewCommentRepository(db),
+	}
+}
+
+type PostRepository struct {
 	DB *gorm.DB
 }
 
-func FunctionOfRepository(db *gorm.DB) *Repository {
-	return &Repository{DB: db}
+func NewPostRepository(db *gorm.DB) *PostRepository {
+	return &PostRepository{DB: db}
 }
 
-func (r *Repository) Create(post *models.Post) error {
+func (r *PostRepository) Create(post *models.Post) error {
 	return r.DB.Create(post).Error
 }
 
-func (r *Repository) FindAll(posts *[]models.Post) error {
-	return r.DB.Find(posts).Error
+func (r *PostRepository) FindAll(posts *[]models.Post) error {
+	return r.DB.Preload("Comments").Find(posts).Error
 }
 
-func (r *Repository) FindByID(id string, post *models.Post) error {
+func (r *PostRepository) FindByID(id string, post *models.Post) error {
 	return r.DB.First(post, id).Error
 }
 
-func (r *Repository) Update(post *models.Post) error {
+func (r *PostRepository) Update(post *models.Post) error {
 	return r.DB.Save(post).Error
 }
 
-func (r *Repository) Delete(id string) error {
+func (r *PostRepository) Delete(id string) error {
 	return r.DB.Delete(&models.Post{}, id).Error
+}
+
+type CommentRepository struct {
+	DB *gorm.DB
+}
+
+func NewCommentRepository(db *gorm.DB) *CommentRepository {
+	return &CommentRepository{DB: db}
+}
+
+func (r *CommentRepository) Create(comment *models.Comment) error {
+	return r.DB.Create(comment).Error
+}
+
+func (r *CommentRepository) FindAllByPostID(postID string, comments *[]models.Comment) error {
+	return r.DB.Where("post_id = ?", postID).Find(comments).Error
+}
+
+func (r *CommentRepository) FindByID(id string, comment *models.Comment) error {
+	return r.DB.First(comment, id).Error
+}
+
+func (r *CommentRepository) Update(comment *models.Comment) error {
+	return r.DB.Save(comment).Error
+}
+
+func (r *CommentRepository) Delete(id string) error {
+	return r.DB.Delete(&models.Comment{}, id).Error
 }
